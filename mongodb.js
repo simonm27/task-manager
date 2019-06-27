6247a6cbbc88ea5d47c8139847e718a620afa376
Simon Minifie <simon.minifie@local.informance.co.uk>
2019-06-27 15:57:43 +0100

    changes

--- mongodb.js
+++ mongodb.js
@@ -9,10 +9,6 @@ const {MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
@@ -20,52 +16,30 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Paul',
-        age: 32
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
-        console.log(result.ops)
+    // db.collection('users').findOne({_id: new ObjectID("5d0cc8ef68e7dfb3840ea4bd")}, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+    //     console.log(user)
+    // })
+
+    db.collection('users').find({age: 44}).toArray((error, users) => {
+        console.log(users)
     })
 
-    // db.collection('users').insertMany ([
-    //     {
-    //         name: 'Leila',
-    //         age: 41
-    //     },
-    //     {
-    //         name: 'Megan',
-    //         age: 13
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert users')
-    //     }
-    //     console.log(result.ops)
+    // db.collection('users').find({age: 44}).count((error, count) => {
+    //     console.log(count)
     // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Mow the lawn',
-    //         completed: true
-    //     },
-    //     {
-    //         description: 'Flea the cat',  
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'Vacuum',
-    //         completed: true
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('There was an error inserting new tasks')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection('tasks').findOne({_id: new ObjectID("5d0cca13eab3e9b748f54b51")}, (error, task) => {
+        if (error) {
+            return console.log('unable to fetch')
+        }
+            console.log(task)
+    })
 
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        console.log(tasks)
+    }) 
 
 })
