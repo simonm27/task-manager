fd8a25dd834313c85a615f45a0580b99243d83f9
Simon Minifie <simon@minifie.me>
2019-07-16 16:46:34 +0100

    changes to course material

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
 //CRUD - Create, read, update, delete
 
-const mongodb = require('mongodb') //Mongodb driver
-const MongoClient = mongodb.MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -13,49 +12,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Simon',
+    // db.collection('users').deleteMany({
     //     age: 44
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Megan',
-    //         age: 13
-    //     },
-    //     {
-    //         name: 'Lauren',
-    //         age: 10
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents')
-    //     }
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Mow the lawn',
-            completed: true
-        },
-        {
-            description: 'Feed the cat',
-            completed: false
-        },
-        {
-            description: 'Do the washing up',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents')
-        }
-        console.log(result.ops)
+    db.collection('tasks').deleteOne({
+        description: "Mow the lawn"
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
+
 })
---
+++ src/db/mongoose.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        validate(value) {
+            if (value.includes('password')) {
+                throw new Error (`Password can't contain the word 'password'`)
+            }
+            }
+        }
+    }
+)
+
+const me = new User({
+    name: ' Lauren  ',
+    email: 'LaureN@MINifie.Me',
+    password: 'seventeen'
+})
+
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
+
+const Tasks = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+// const task  = new Tasks({
+//     description: 'Mow the Lawn',
+//     completed: false
+// })
+
+// task.save().then((task) => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
