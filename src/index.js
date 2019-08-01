c3d71648b3d72a27699f86b77beb46a71944934b
Simon Minifie <simon@minifie.me>
2019-08-01 09:17:35 +0100

    course continues

--- src/index.js
+++ src/index.js
@@ -13,5 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -9,8 +9,16 @@ router.post('/users', async (req, res) => {
     try{
         await user.save()
         res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
     }
-    catch (e) {
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
         res.status(400).send(e)
     }
 })
@@ -19,8 +27,7 @@ router.get('/users', async (req,res) => {
     try {
         const users = await User.find({})
         res.send(users)
-    }
-    catch (e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
@@ -52,7 +59,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 //
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
         if (!user) {
             return res.status(404).send()
         }
