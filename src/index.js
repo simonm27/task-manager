3ad9d2b115067b92feb03ed5fd0d5f9f286598a9
Simon Minifie <simon@minifie.me>
2019-08-02 14:45:53 +0100

    course changes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,39 @@ const taskRouter = require('./routers/task.js')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//        res.send('GET requests are disabled') 
+//     }
+//     else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+        res.status(503).send('Site is in maintenance mode. Check back soon')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'acb123' }, 'thisisthenodejscourse', { expiresIn: '20 second' })
+//     console.log(token)
+
+//     const data = jwt.verify(token, 'thisisthenodejscourse')
+
+//     console.log(data)
+// }
+
+// myFunction()
